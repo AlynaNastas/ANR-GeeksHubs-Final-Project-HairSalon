7fcd67121405e400c04ebe91f7bb12cafbd3542e
Alyna Nastas Romaniuc <nastasalyna@gmail.com>
2023-04-23 20:27:48 +0200

    fix: navbar was not working correctly. Still need to add some more functions as login as stylist and user. But now at least enters as Admin and Admin's endpoints

--- HairSalon/src/components/Navbar/NavbarPage.jsx
+++ HairSalon/src/components/Navbar/NavbarPage.jsx
@@ -3,65 +3,126 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { useDispatch, useSelector } from "react-redux";
+import { userData, userout } from "../../userSlice";
 
 
 
 
 export const NavbarPage = () => {
-  return (
-    <Navbar bg="dark" expand="lg" variant="dark">
-    <Container>
-        <Navbar.Brand as={Link} to="/home">Art&Beauty AN</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto"> 
-        <>
-            <Nav.Link as={Link} to="/register">
-                Register
-        </Nav.Link>
-            <Nav.Link as={Link} to="/login">
-                Login
-            </Nav.Link>
-        <Nav.Link as={Link} to="/">
-                Home
-        </Nav.Link>
-        <Nav.Link as={Link} to="/viewusers">
-                View users
-        </Nav.Link>
-        <Nav.Link as={Link} to="/profile">
-                Profile
-        </Nav.Link>
-            <NavDropdown title="About" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
-                    Salon
-                </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-                        Team
-                </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">
-                        Treatments
-                </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.4">
-                        Services
-                </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.5">
-                        Our History
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.6">
-                        News
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.7">
-                        Reviews
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.7">
-                        Contact
-                </NavDropdown.Item>
-            <NavDropdown.Divider />
-    </NavDropdown>
-            </>
-        </Nav>
-    </Navbar.Collapse>
-</Container>
-</Navbar>
-  )
+
+    const dispatch = useDispatch();
+
+    const logout = () => {
+        dispatch(userout({ credentials: {}, token: "" }));
+    };
+
+    const dataCredentialsRdx = useSelector(userData);
+
+    if (!dataCredentialsRdx.credentials.usuario) {
+
+        return <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/home">Art&Beauty AN</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <>
+                            <Nav.Link as={Link} to="/register">
+                                Register
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/">
+                                Home
+                            </Nav.Link>
+
+                            <NavDropdown title="About" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">
+                                    Salon
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Team
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Treatments
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">
+                                    Services
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.5">
+                                    Our History
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.6">
+                                    News
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.7">
+                                    Reviews
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.7">
+                                    Contact
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                            </NavDropdown>
+                        </>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    }
+    if (dataCredentialsRdx.credentials.usuario.roles.includes('Admin')) {
+
+        return <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/home">Art&Beauty AN</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <>
+                            <Nav.Link as={Link} to="/">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/viewusers">
+                                View users
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/profile">
+                                Profile
+                            </Nav.Link>
+                            <NavDropdown title="About" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">
+                                    Salon
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Team
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">
+                                    Treatments
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">
+                                    Services
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.5">
+                                    Our History
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.6">
+                                    News
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.7">
+                                    Reviews
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.7">
+                                    Contact
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                            </NavDropdown>
+                        </>
+                        <Nav.Link onClick={logout} as={Link} to="/home">Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    }
 }
+
